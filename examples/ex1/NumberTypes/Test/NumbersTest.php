0f687c2d99888866ec0936f937b72feeb5d9cda7
Craig Mayhew <craig.mayhew@adire.co.uk>
2014-04-20 16:57:14 +0100

    Cut down test case to be much simpler for now.

--- examples/ex1/NumberTypes/Test/NumbersTest.php
+++ examples/ex1/NumberTypes/Test/NumbersTest.php
@@ -11,44 +11,15 @@ class NumbersTest extends \PHPUnit_Framework_TestCase
     public function isEven($i)
     {
         $this->assertEquals(0, $i%2);
+        $o = new NumberTypes\Numbers();
+var_dump($o);
+	$this->assertEquals(true, $o->isEven($i));
     }
     public function providerEven(){
         return array(
             array(2),
             array(246),
             array(284628)
-        );
-    }
-    /**
-     * @test
-     * @dataProvider providerOdd
-     */
-    public function isOdd($i)
-    {
-        $this->assertEquals(1, $i%2);
-    }
-    public function providerOdd(){
-        return array(
-            array(1),
-            array(3),
-            array(2461),
-            array(2846287)
-        );
-    }
-    /**
-     * @test
-     * @dataProvider providerPrime
-     */
-    public function isPrime($i)
-    {
-        $this->assertEquals(true, gmp_prob_prime($i));
-    }
-    public function providerPrime(){
-        return array(
-            array(3),
-            array(7),
-            array(13),
-            array(29)
         );
     }
 }
